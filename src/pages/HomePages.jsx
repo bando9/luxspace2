a3bb72a69990259ef7c9fc4011ce7334c37bba0a
bando9 <bandomega1@gmail.com>
2025-02-27 21:45:28 +0700

    feat: install react-router-dom and add Header section component

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import Hero from "../parts/Hero";
+import Header from "../parts/Header";
+
+export default function HomePages() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
